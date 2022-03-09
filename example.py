26f85fd04d357316c3350315d221be638a1d7c4d
YeetDisDude <66558817+YeetDisDude@users.noreply.github.com>
2022-03-09 12:56:16 +0700

    Create example.py

---
+++ example.py
@@ -0,0 +1,8 @@
+import haxxorman
+
+person = "YeetDisDude#001"
+haxxorman.getEmail(person)
+haxxorman.getIP(person)
+haxxorman.getAddress(person)
+haxxorman.getName(person)
+haxxorman.sendPizza(person, 999)
